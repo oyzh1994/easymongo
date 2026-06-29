aa2303d6d21fc3ce645529c1c67e28eb179ebfff
oyzh <oyzh.1994@q.qcom>
2026-06-29 18:48:45 +0800

    update: 统一优化调整

--- src/main/java/cn/oyzh/easymongo/trees/MongoTreeItemFilter.java
+++ src/main/java/cn/oyzh/easymongo/trees/MongoTreeItemFilter.java
@@ -9,7 +9,7 @@ import cn.oyzh.fx.gui.tree.view.RichTreeItemFilter;
  * @author oyzh
  * @since 2023/06/30
  */
-public class MongoTreeItemFilter implements RichTreeItemFilter {
+public class MongoTreeItemFilter extends RichTreeItemFilter {
 
     /**
      * 仅看收藏键
